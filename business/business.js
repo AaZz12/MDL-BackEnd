bc745aefd4255b6de511568c69b28a9a76a35a6e
AaZz <theaazz@outlook.com>
2023-03-20 18:39:24 +0100

    squelette business

---
+++ business/business.js
@@ -0,0 +1,25 @@
+/*Importation des modules*/
+const data = require("../data/data");
+
+/*Fonctions exportées*/
+const business_public = {
+    get_all_users : (req, res, next)=>{
+
+    },
+    add_user : (req, res, next)=>{
+
+    },
+    edit_user : (req, res, next)=>{
+
+    },
+    delete_user : (req, res, next)=>{
+        
+    }
+
+    
+};
+
+
+/*Exportation comme module */
+
+module.exports = business_public;
---
+++ data/data.js
@@ -0,0 +1,13 @@
+/*Importation des modules*/
+
+
+/*Initialisation de l'application*/
+
+
+/*API BackEnd*/
+
+
+
+/*Exportation comme module */
+
+module.exports={};
