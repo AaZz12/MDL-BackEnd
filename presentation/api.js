77c2fa45dd1e6512b63d61766df0e62a826ed854
AaZz <theaazz@outlook.com>
2023-03-20 18:32:57 +0100

    ajout des fonctions de bases dans API

--- presentation/api.js
+++ presentation/api.js
@@ -1,4 +1,34 @@
 /*Importation des modules*/
 const express = require("express");
-const business = require("../business/business.js");
+const business = require("../business/business");
 
+/*Initialisation de l'application*/
+let app = express();
+const REQUESTS_URL="/users";
+
+/*API BackEnd*/
+const api = {
+    /**
+     * Start the api on given port
+     * @param {number} port The port identifier
+     */
+    start : port=>{
+        //Autorisation des reqêtes JSON
+        app.use(express.json());
+
+        //Toutes les requêtes
+        app.get(REQUESTS_URL, business.get_all_users);
+        app.post(REQUESTS_URL, business.add_user);
+        app.put(REQUESTS_URL, business.edit_user);
+        app.delete(REQUESTS_URL, business.delete_user);
+
+        app.listen(port, ()=>{
+            console.log(`App listening to port ${port}`);
+        });
+    }
+};
+
+
+/*Exportation comme module */
+
+module.exports={api};
